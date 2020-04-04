c686dfe2eacefc4dad4fd538e5afcc06bfed3b6c
JP Sheehan <jesse@sheehan.nz>
2020-04-04 14:27:38 +1300

    Decomposed colors and utilities

--- main.py
+++ main.py
@@ -1,140 +1,24 @@
 import numpy as np
 import cv2 as cv
-import math
-import os
-import os.path
-import time
-import random
 
-CAMERA_SURFACE_FRONT = 0
-CAMERA_SURFACE_BACK = 1
-CAMERA_LOGITECH = 2
+from utilities import *
+from colors import *
 
 
-class Camera:
-    """ Provides an interface to read undistorted camera frames. """
+class OpenCVGui:
 
-    def __init__(self, camera_id, camera_matrix_file='./camera_matrix.npy', distortion_coeff_file='./distortion_coeff.npy'):
-        """ Initialises the camera with distortion information. """
+    def __init__(self):
+        self.name = "Nice Dice Advice Device"
 
-        # open video capture device
-        self.cap = cv.VideoCapture(camera_id)
-
-        # open files containing distortion information
-        self.camera_matrix = np.loadtxt(camera_matrix_file)
-        self.distortion_coeff = np.loadtxt(distortion_coeff_file)
-
-        # read a single frame to get the width and height
-        _ret, frame = self.cap.read()
-        self.frame_height, self.frame_width = frame.shape[:2]
-
-        # create a new camera matrix and distortion
-        self.new_camera_matrix, self.roi = cv.getOptimalNewCameraMatrix(
-            self.camera_matrix, self.distortion_coeff, (self.frame_width, self.frame_height), 1, (self.frame_width, self.frame_height))
-        _mapx, _mapy = cv.initUndistortRectifyMap(
-            self.camera_matrix, self.distortion_coeff, None, self.new_camera_matrix, (self.frame_width, self.frame_height), cv.CV_16SC2)
-
-    def __del__(self):
-        """ Releases the camera. """
-        self.cap.release()
-
-    def read(self):
-        """ Returns an undistorted and cropped frame from the camera. """
-        # get a frame from the camera
-        _ret, raw_img = self.cap.read()
-
-        # undistort the frame
-        processed_img = cv.undistort(raw_img, self.camera_matrix,
-                                     self.distortion_coeff, None, self.new_camera_matrix)
-
-        # crop the frame
-        x, y, w, h = self.roi
-        processed_img = processed_img[y:y+h, x:x+w]
-
-        return processed_img
-
-
-class Die:
-
-    def __init__(self, img):
-        self.img = img
-        self.value = None
-
-    def __str__(self):
-        if self.value is None:
-            return "?"
-        else:
-            return str(self.value)
-
-    def compute_value(self):
+    def _nop(self):
         pass
 
+    def run(self):
+        cv.namedWindow(self.name)
+        cv.createButton("Save Images", self.on_button_save)
 
-def cropCopy(img, x, y, w, h):
-    """ Returns a cropped copy of the image. """
-    return img[y:y+h, x:x+w].copy()
-
-
-def crop(img, x, y, w, h):
-    """ Returns the cropped image. """
-    return img[y:y+h, x:x+w]
-
-
-def isolate_die_face(img, rect, angle):
-    """ Isolates the die face from the rest of the image. """
-    [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = rect
-
-    xs = [x1, x2, x3, x4]
-    ys = [y1, y2, y3, y4]
-
-    # the width and height are the differences between the maximum and minimum coordinates
-    w = max(xs) - min(xs)
-    h = max(ys) - min(ys)
-
-    if w == 0 or h == 0:
-        return None
-
-    # crop out our unrotated die face
-    rot_img = cropCopy(img, min(xs), min(ys), w, h)
-
-    if len(rot_img) == 0:
-        return None
-
-    # rotate the die face so it is the right way up
-    rot_mat = cv.getRotationMatrix2D((int(w / 2), int(h / 2)), angle, 1.0)
-    rot_img = cv.warpAffine(rot_img, rot_mat, (w, h))
-
-    # now the rotated die has some padding from the rotation
-    # calculate the width of the padding and crop it out
-    rot_w = int(math.sqrt((x3 - x2) ** 2 + (y3 - y2) ** 2))
-    rot_h = int(math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))
-    rot_img = crop(rot_img, int((w - rot_w) / 2),
-                   int((h - rot_h) / 2), rot_w, rot_h)
-
-    return rot_img
-
-
-def save_die_face(img, folder='die_images'):
-    """ Saves the die face to disk. """
-    if not os.path.exists(folder):
-        os.mkdir(folder)
-
-    # generate a unique filename for the image
-    filename = None
-    while True:
-        filename = str(int(time.time() * 1000)) + "_" + \
-            str(random.randint(0, 10000)) + ".png"
-        filename = os.path.join(folder, filename)
-        if not os.path.exists(filename):
-            break
-
-    cv.imwrite(filename, img)
-
-
-def pipeline(img, *fns):
-    for fn in fns:
-        img = fn(img)
-    return img
+    def on_button_save(self):
+        pass
 
 
 def greyscale(img):
@@ -144,19 +28,31 @@ def greyscale(img):
 
 def blur(img):
     # img = cv.GaussianBlur(img, (3, 3), 0)
-    img = cv.medianBlur(img, 5)
-    # img = cv.bilateralFilter(img, 3, 150, 150)
+    # img = cv.medianBlur(img, 3)
+    img = cv.bilateralFilter(img, 3, 150, 150)
     return img
 
 
 def threshold(img):
-    _ret, img = cv.threshold(img, 100, 255, cv.THRESH_BINARY)
+    # _ret, img = cv.threshold(img, 90, 128, cv.THRESH_BINARY)
     return img
 
 
 def edges(img):
-    img = cv.Canny(img, 200, 400)
+    img = cv.Canny(img, 100, 400)
     return img
+
+
+def get_contours(img):
+    # find all the contours
+    contours, _ = cv.findContours(
+        img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    # filter out small contours outside of our expected size range
+    contours = filter(lambda c: cv.contourArea(
+        c) > 1000 and cv.contourArea(c) < 3500, contours)
+
+    return contours
 
 
 def main():
@@ -174,20 +70,11 @@ def main():
         img_processed = pipeline(img, greyscale, threshold, blur, edges)
 
         # detect contours
-        contours, _ = cv.findContours(
-            img_processed, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-
-        # filter out small contours
-        contours = list(filter(lambda c: cv.contourArea(c) > 100, contours))
+        contours = get_contours(img_processed)
 
         die_faces = []
 
-        # filter contours by area
-        contours = filter(lambda c: cv.contourArea(c) > 500, contours)
-
         for i, contour in enumerate(contours):
-
-            # print(i, cv.contourArea(contour))
 
             _, _, angle = rot_rect = cv.minAreaRect(contour)
             rot_rect = cv.boxPoints(rot_rect)
@@ -201,14 +88,19 @@ def main():
                 continue
 
             img_annotated = cv.drawContours(
-                img_annotated, [rot_rect], 0, (255, 0, 0), 2)
+                img_annotated, [rot_rect], 0, COLOR_RED, 2)
 
             img_annotated = cv.putText(
-                img_annotated, "ID: {}".format(i + 1), (rect[0] + rect[2] + 5, rect[1] + 5), cv.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
+                img_annotated, "ID: {}".format(i + 1), (rect[0] + rect[2] + 5, rect[1] + 5), cv.FONT_HERSHEY_PLAIN, 1, COLOR_GREEN)
 
             die_faces.append(die_face)
 
-            # cv.imshow(str(i), die_face)
+        # print the number of dice found on the image
+        noun = "dice"
+        if len(die_faces) == 1:
+            noun = "die"
+        cv.putText(img_annotated, "Found {} {}!".format(str(len(die_faces)), noun),
+                   (0, img_annotated.shape[0]), cv.FONT_HERSHEY_PLAIN, 3, COLOR_WHITE, 2)
 
         cv.imshow('Processed', img_processed)
         cv.imshow('Original with Contours', img_annotated)
@@ -218,8 +110,6 @@ def main():
         elif cv.waitKey(1) & 0xff == ord(' '):
             for die_face in die_faces:
                 save_die_face(die_face)
-            # cv.displayOverlay("Original with Contours", "Saved {} images".format(
-            #     str(len(die_faces))), 2000)
             print("Saved {} images".format(str(len(die_faces))))
 
     cv.destroyAllWindows()
---
+++ utilities.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+
+import numpy as np
+import cv2 as cv
+import math
+import os
+import os.path
+import time
+import random
+
+CAMERA_SURFACE_FRONT = 0
+CAMERA_SURFACE_BACK = 1
+CAMERA_LOGITECH = 2
+
+
+class Camera:
+    """ Provides an interface to read undistorted camera frames. """
+
+    def __init__(self, camera_id, camera_matrix_file='./camera_matrix.npy', distortion_coeff_file='./distortion_coeff.npy'):
+        """ Initialises the camera with distortion information. """
+
+        # open video capture device
+        self.cap = cv.VideoCapture(camera_id)
+
+        # open files containing distortion information
+        self.camera_matrix = np.loadtxt(camera_matrix_file)
+        self.distortion_coeff = np.loadtxt(distortion_coeff_file)
+
+        # read a single frame to get the width and height
+        _ret, frame = self.cap.read()
+        self.frame_height, self.frame_width = frame.shape[:2]
+
+        # create a new camera matrix and distortion
+        self.new_camera_matrix, self.roi = cv.getOptimalNewCameraMatrix(
+            self.camera_matrix, self.distortion_coeff, (self.frame_width, self.frame_height), 1, (self.frame_width, self.frame_height))
+        _mapx, _mapy = cv.initUndistortRectifyMap(
+            self.camera_matrix, self.distortion_coeff, None, self.new_camera_matrix, (self.frame_width, self.frame_height), cv.CV_16SC2)
+
+    def __del__(self):
+        """ Releases the camera. """
+        self.cap.release()
+
+    def read(self):
+        """ Returns an undistorted and cropped frame from the camera. """
+        # get a frame from the camera
+        _ret, raw_img = self.cap.read()
+
+        # undistort the frame
+        processed_img = cv.undistort(raw_img, self.camera_matrix,
+                                     self.distortion_coeff, None, self.new_camera_matrix)
+
+        # crop the frame
+        x, y, w, h = self.roi
+        processed_img = processed_img[y:y+h, x:x+w]
+
+        return processed_img
+
+
+class Die:
+
+    def __init__(self, img):
+        self.img = img
+        self.value = None
+
+    def __str__(self):
+        if self.value is None:
+            return "?"
+        else:
+            return str(self.value)
+
+    def compute_value(self):
+        pass
+
+
+def cropCopy(img, x, y, w, h):
+    """ Returns a cropped copy of the image. """
+    return img[y:y+h, x:x+w].copy()
+
+
+def crop(img, x, y, w, h):
+    """ Returns the cropped image. """
+    return img[y:y+h, x:x+w]
+
+
+def isolate_die_face(img, rect, angle):
+    """ Isolates the die face from the rest of the image. """
+    [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = rect
+
+    xs = [x1, x2, x3, x4]
+    ys = [y1, y2, y3, y4]
+
+    # the width and height are the differences between the maximum and minimum coordinates
+    w = max(xs) - min(xs)
+    h = max(ys) - min(ys)
+
+    if w == 0 or h == 0:
+        return None
+
+    # crop out our unrotated die face
+    rot_img = cropCopy(img, min(xs), min(ys), w, h)
+
+    if rot_img.size == 0:
+        return None
+
+    # rotate the die face so it is the right way up
+    rot_mat = cv.getRotationMatrix2D((int(w / 2), int(h / 2)), angle, 1.0)
+    rot_img = cv.warpAffine(rot_img, rot_mat, (w, h))
+
+    # now the rotated die has some padding from the rotation
+    # calculate the width of the padding and crop it out
+    rot_w = int(math.sqrt((x3 - x2) ** 2 + (y3 - y2) ** 2))
+    rot_h = int(math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))
+    rot_img = crop(rot_img, int((w - rot_w) / 2),
+                   int((h - rot_h) / 2), rot_w, rot_h)
+
+    return rot_img
+
+
+def save_die_face(img, folder='die_images'):
+    """ Saves the die face to disk. """
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+    # generate a unique filename for the image
+    filename = None
+    while True:
+        filename = str(int(time.time() * 1000)) + "_" + \
+            str(random.randint(0, 10000)) + ".png"
+        filename = os.path.join(folder, filename)
+        if not os.path.exists(filename):
+            break
+
+    cv.imwrite(filename, img)
+
+
+def pipeline(img, *fns):
+    """ Allows for chaining of image processing functions without intermediate variables. """
+    for fn in fns:
+        img = fn(img)
+    return img
